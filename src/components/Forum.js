8abcf66cedecb247f4bc04f937d28d0f45725448
aryan-prog <mandelakalam12364@gmail.com>
2022-07-04 18:07:59 +0530

    revamped response card

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,7 +1,9 @@
 import React from "react";
 import CommentBox from "./CommentBox.js";
 import OldOpportunityCard from './OldOpportunityCard.js';
+import ResponseCard from "./ResponseCard.js";
 import "../css/Forum.css";
+
 
 
 const Forum=()=>{
@@ -11,6 +13,9 @@ const Forum=()=>{
             <div class="number-of-responses">1 response</div>
             <hr/>
             <CommentBox/>
+            <br/>
+            <br/>
+            <ResponseCard/>
         </div>
     )
 }
