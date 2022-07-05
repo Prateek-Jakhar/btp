c1d93ebe12dd8121792d7eadae2cbe7a56596b86
Aditya Gupta <adityagupta765469@gmail.com>
2022-07-05 20:53:11 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import "./App.css";
-//import FeedButton from "./components/FeedButton";
-import HeaderOptions from "./components/HeaderOptions";
+// import FeedButton from "./components/FeedButton";
+// import HeaderOptions from "./components/HeaderOptions";
 // import LogIn from "./components/LogIn";
 // import NewOpportunityCard from "./components/NewOpportunityCard";
-// import NewOpportunityCardTop from "./components/NewOpportunityCardTop";
-//import OldOpportunityCard from "./components/OldOpportunityCard";
-import Forum from "./components/Forum.js";
-import LeftMenu from "./components/LeftMenu";
-//import LeftHeader from "./components/LeftHeader";
-import PeopleCard from "./components/PeopleCard.js";
-import PeopleList from "./components/PeopleCardList";
-import People from "./People";
+import NewOpportunityCardTop from "./components/NewOpportunityCardTop";
+// import OldOpportunityCard from "./components/OldOpportunityCard";
+// import LeftHeader from "./components/LeftHeader";
+import Dashboard from "./components/Dashboard";
+import People from "./components/pages/People";
 
 function App() {
   return (
-    <div className="App">
-      {/* <LogIn /> */}
-      {/* <NewOpportunityCard /> */}
-      {/* <NewOpportunityCardTop /> */}
-      {/*<OldOpportunityCard />*/}
-      <Forum/>
-      {/*<PeopleCard />*/}
-      {/*<PeopleList/>*/}
-      {/* <FeedButton text={"Discover"} /> */}
-      {/* {<LeftHeader />} */}
-      {/*<LeftMenu />*/}
+    <div className="App"> 
+      
+     { /* <LogIn /> */ } {
+      /* <NewOpportunityCard /> */ } {
+      /* <NewOpportunityCardTop /> */ } {
+      /*<OldOpportunityCard />*/ } <
+    Forum / > {
+      /*<PeopleCard />*/ } {
+      /*<PeopleList/>*/ } {
+      /* <FeedButton text={"Discover"} /> */ } {
+      /* {<LeftHeader />} */ } {
+      /*<LeftMenu />*/}
     </div>
   );
 }
