b3462f8c4c6af76c267aa58e1d732ac698fd7f42
aryan-prog <mandelakalam12364@gmail.com>
2022-07-11 17:08:33 +0530

    made profile button made applications page

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -2,17 +2,30 @@ import React from "react";
 import OldOpportunityCard from "./OldOpportunityCard";
 import "../css/Applications.css";
 import NewOpportunityCard from "./NewOpportunityCard";
+import ProfileButton from "./ProfileButton";
 
 const Applications=()=>{
     return(
         <div className="main-applications" >
-            <NewOpportunityCard/>
-            <NewOpportunityCard/>
-            <br/>
-            <NewOpportunityCard/>
-            <br/>
-            {/* <OldOpportunityCard/>
-            <br/> */}
+            <div className="application-header">
+                <span className="on-campus">
+                    <text>On Campus</text>
+                </span>
+                <span className="apply-filters">
+                    <text>Apply Filters</text>
+                </span>
+                <ProfileButton/>
+            </div>
+            <div className="application-cards">
+                <NewOpportunityCard />
+                <br />
+                <NewOpportunityCard />
+                <br />
+                <NewOpportunityCard />
+                <br />
+                
+            </div>
+            <text className="finish">Yay, You have seen it all!</text>
         </div>
     )
 }
