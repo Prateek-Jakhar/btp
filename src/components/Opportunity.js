bd49f6785665cbdaccd7f550e11edeb7dc6aa04e
aryan-prog <mandelakalam12364@gmail.com>
2022-07-11 17:10:27 +0530

    made Opportunity Page

---
+++ src/components/Opportunity.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Applications from "./Applications";
+
+const Opportunity=()=>{
+    return(
+        <div>
+            <Applications/>
+        </div>
+    )
+}
+
+export default Opportunity;
