7b2dbc5461cc44b29f82cf58186678a28775c173
jtushar3011 <jtushar3011@gmail.com>
2022-11-15 09:38:19 +0530

    Added firestore for people route

--- src/components/PeopleCard.js
+++ src/components/PeopleCard.js
@@ -2,20 +2,26 @@ import React from "react";
 import "../css/PeopleCard.css";
 import pic from "../assets/pic.png";
 import msg from "../assets/msg-icon.png"
+import db from "./Firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { listClasses } from "@mui/material";
 
 
-const PeopleCard = () => {
+
+const PeopleCard = ({info}) => {
+    // console.log(info);
+    // { name: "Aryan Seth", department: "CSE", resume_link: "", batch_year: "Y19", user_image_link: "", phone_number: "8299821151", user_id: "001", program: "B.Tech" }
     return (
         <div className="main">
             <div className="student-details">
                 <div className="student-name">
-                    <p>Pranjay Choudhary</p>
+                    <p>{info.name}</p>
                 </div>
                 <div className="branch">
-                    <p>B.tech-Mechanical</p>
+                    <p>{info.program}-{info.department}</p>
                 </div>
                 <div className="batch">
-                    <p>Student-Batch of '23</p>
+                    <p>Student-Batch of '{info.batch_year.slice(-2)}</p>
                 </div>
             </div>
             <div className="photo">
